848256d7aa3f70e6b874f256a65f0d97f567d816
sindydwns <sindydwns@naver.com>
2022-08-24 03:53:10 +0900

    update test.js: add user location api

--- Routers/test.js
+++ Routers/test.js
@@ -1,8 +1,20 @@
 import app from "express";
+import api42 from "../api42.js";
 
 const router = app.Router();
-router.get("/", (req, res, next) => {
-	res.sendStatus(200);
+router.get("/:user_id/location", async (req, res, next) => {
+	const userId = req.params.user_id;
+	const path = `/v2/users/${userId}/locations`;
+	const config = {params:{"page[size]":1}};
+	try {
+		const data = await api42("GET", path, config);
+		const lastLocation = data[0];
+		const user = lastLocation?.user;
+		res.json({
+			last: lastLocation?.host,
+			current: user?.location,
+		});
+	} catch(e) {next(e);}
 });
 
 export default router;
