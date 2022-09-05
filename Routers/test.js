c94f7abf8efebe5dddedb31ed6fa19907987ae09
sindydwns <sindydwns@naver.com>
2022-09-05 14:28:48 +0900

    temp

--- Routers/test.js
+++ Routers/test.js
@@ -7,21 +7,49 @@ import { postDM2User, postDM2Channel, reactDM } from "../apiSlack.js";
 const router = app.Router();
 
 router.get("/:user_id/location", async (req, res, next) => {
-    const userId = req.params.user_id;
-    const path = `/v2/users/${userId}/locations`;
-    const config = { params: { "page[size]": 1 } };
-    try {
-        // getUserInfo(id);
-        const data = await api42("GET", path, config);
-        const lastLocation = data[0];
-        const user = lastLocation?.user;
-        res.json({
-            last: lastLocation?.host,
-            current: user?.location,
-        });
-    } catch (e) {
-        next(e);
-    }
+	const userId = req.params.user_id;
+	const path = `/v2/users/${userId}/locations`;
+	const config = {params:{"page[size]":1}};
+	try {
+		const data = await api42("GET", path, config);
+		const lastLocation = data[0];
+		const user = lastLocation?.user;
+		res.json(data);
+	} catch(e) {next(e);}
+});
+
+let last = null;
+router.get("/campus", async (req, res, next) => {
+	const userId = req.params.user_id;
+	const campusId = 29;
+	const path = `/v2/campus/${campusId}/locations`;
+	try {
+		let total = [];
+		const now = new Date();
+		const begin = last ?? new Date(now - (1000 * 60 * 60 * 24));
+		for (let i = 0; i < 20; i++) {
+			const config = {params:{
+				"page[size]": "100",
+				"page[number]": i + 1,
+				// "range[begin_at]": `${begin.toISOString()},${now.toISOString()}`,
+				"filter[active]": true,
+				"filter[primary]": true,
+				"range[host]": "c10,c9r9s9"
+			}};
+			const data = await api42("GET", path, config);
+			if (data.length == 0)
+				break;
+			total = [...total, ...data];
+		}
+		last = now;
+		total = total.map(x => ({
+			userId: x.user.login,
+			host: x.host,
+			begin_at: x.begin_at,
+		}))
+		// res.json(total.map(x => ({host:x.host, user:x.user.id, user_id:x.user.login, begin: x.begin_at, end: x.end_at})));
+		res.json({len:total.length, total});
+	} catch(e) {next(e);}
 });
 
 router.get("/userInfo/:user_id", async(req, res, next) => {
