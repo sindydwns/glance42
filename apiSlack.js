d2f6687f587f5c3aa77c8289ed3f69f5e88d4558
situso <situso.dv@gmail.com>
2022-09-28 02:41:09 +0900

    add slack boltpackage temp

--- apiSlack.js
+++ apiSlack.js
@@ -1,62 +1,105 @@
 import dotenv from "dotenv";
 import { WebClient } from "@slack/web-api";
+import pkg from "@slack/bolt";
 
+const { App } = pkg;
 dotenv.config();
 const web = new WebClient(process.env.SLACK_TOKEN);
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true, // add this
+    appToken: process.env.SLACK_APP_TOKEN, // add this
+    port: process.env.PORT || 3000,
+});
+
+app.message("hello", async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say({
+        blocks: [
+            {
+                type: "section",
+                text: {
+                    type: "mrkdwn",
+                    text: `Hey there <@${message.user}>!`,
+                },
+                accessory: {
+                    type: "button",
+                    text: {
+                        type: "plain_text",
+                        text: "Click Me",
+                    },
+                    action_id: "button_click",
+                },
+            },
+        ],
+        text: `Hey there <@${message.user}>!`,
+    });
+});
+
+app.action("button_click", async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    await say(`<@${body.user.id}> clicked the button`);
+});
+
+(async () => {
+    // Start your app
+    await app.start();
+
+    console.log("⚡️ Bolt app is running!");
+})();
 
 async function errorHandler(fn) {
-	try {
-		return await fn()
-	} catch(e) {throw new SlackError(e);}
+    try {
+        return await fn();
+    } catch (e) {
+        throw new SlackError(e);
+    }
 }
 
 export default web;
 
-export async function getUserList() {
-	return await errorHandler(async () => web.users.list());
-}
-
 /**
  * post direct message to user
  * @param {string} userId example XXXXXXXXXXX
- * @param {string} text 
+ * @param {string} text
  */
 export async function postDM2User(users, text) {
-	return await errorHandler(async () => {
-		const conversation = await web.conversations.open({users})
-		if (!conversation.ok)
-			throw new SlackError("conversation open failed");
-		const channel = conversation.channel.id;
-		return await web.chat.postMessage({channel, text});
-	});
+    return await errorHandler(async () => {
+        const conversation = await web.conversations.open({ users });
+        if (!conversation.ok) throw new SlackError("conversation open failed");
+        const channel = conversation.channel.id;
+        return await web.chat.postMessage({ channel, text });
+    });
 }
 
 export async function postDM2Channel(channel, text) {
-	return await errorHandler(async () => {
-		return await web.chat.postMessage({channel, text});
-	})
+    return await errorHandler(async () => {
+        return await web.chat.postMessage({ channel, text });
+    });
 }
 
 /**
  * add reaction to message
- * @param {string} channel 
- * @param {string} ts 
- * @param {string} name 
- * @returns 
+ * @param {string} channel
+ * @param {string} ts
+ * @param {string} name
+ * @returns
  */
 export async function reactDM(channel, ts, name) {
-	return await errorHandler(async () => {
-		return await web.reactions.add({
-			channel,
-			name,
-			timestamp: ts,
-		});
-	});
+    return await errorHandler(async () => {
+        return await web.reactions.add({
+            channel,
+            name,
+            timestamp: ts,
+        });
+    });
 }
 
 export class SlackError extends Error {
-	constructor(originError, ...params) {
-		super(...params);
-		this.originError = originError;
-	}
+    constructor(originError, ...params) {
+        super(...params);
+        this.originError = originError;
+    }
 }
