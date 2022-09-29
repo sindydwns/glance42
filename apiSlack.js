20b53edf7e577a42c65d4c08e2dd0371bb3a508c
sindydwns <sindydwns@naver.com>
2022-09-29 09:52:25 +0900

    update apiSlack.js

--- apiSlack.js
+++ apiSlack.js
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 import pkg from "@slack/bolt";
 import pageHome from "./slack/pageHome.js";
+import { WebClient } from "@slack/web-api";
 
 const { App, LogLevel } = pkg;
 dotenv.config();
+const web = new WebClient(process.env.SLACK_TOKEN);
 const app = new App({
     token: process.env.SLACK_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -14,6 +16,21 @@ const app = new App({
 });
 
 pageHome(app);
+
+/**
+ * post direct message to user
+ * @param {string} users example XXXXXXXXXXX, XXXXXXXXXXX, XXXXXXXXXXX...
+ * @param {string} text 
+ */
+export async function postDM2User(users, text) {
+	return await errorHandler(async () => {
+		const conversation = await web.conversations.open({users})
+		if (!conversation.ok)
+			throw new SlackError("conversation open failed");
+		const channel = conversation.channel.id;
+		return await web.chat.postMessage({channel, text});
+	});
+}
 
 (async () => {
     await app.start();
