9c87a9e321c6bf7b617b6c9c074e25d736e06ab0
jgo <jingmuio@gmail.com>
2022-11-06 21:35:50 +0900

    [test] home.js

--- Routers/home.js
+++ Routers/home.js
@@ -2,7 +2,7 @@ import app from "express";
 
 const router = app.Router();
 router.get("/", (req, res, next) => {
-	res.send('<h1>glance42 서비스 이용을 위한 인증이 완료되었습니다.<br>이제부터 염탐이 가능합니다. 와!</h1>');
+	res.send('<h1>glance42 서비스 이용을 위한 인증이 완료되었습니다.</h1><h2><br>이제부터 염탐이 가능합니다. 와!</h2>');
 	// res.sendStatus(200);
 });
 
