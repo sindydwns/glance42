899767742dffd8f0c157a9bdbaf2d754e7e6ea75
niamu01 <yeju@student.42seoul.kr>
2023-01-22 19:15:18 +0900

    wip: data

--- src/api/slack/utils/data.js
+++ src/api/slack/utils/data.js
@@ -1,11 +1,32 @@
 import * as dbUser from "../../../DataBase/dbUser.js";
 
+
+양식
+/**
+ * @param {}
+ * @brief 
+ * @return {} 
+ */
+
+
+ /**
+  * @param {} body
+  * @param {} event
+  * @param {} client
+  * @brief body의 user.id를 반환. body자리에 null을 넣으면 event의 user가 반환하는 함수.
+  * @return {} slackId
+  */ 
 export async function getClientSlackId(body, event, client) {
 	const slackId = body ? body.user.id : event.user;
 
 	return slackId;
 }
 
+/**
+ * @param {}
+ * @brief 
+ * @return {} 
+ */
 export async function getClientIntraId(body, event, client) {
 	const slackId = body ? body.user.id : event.user;
 	const seekerId = dbUser.getIntraIdbySlackId(slackId);
@@ -13,6 +34,11 @@ export async function getClientIntraId(body, event, client) {
 	return seekerId;
 }
 
+/**
+ * @param {}
+ * @brief 
+ * @return {} 
+ */
 export async function getUserNamebySlackId(client, slackId) {
 	const userName = await client.users.info({
 		user: slackId,
